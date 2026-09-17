cd482272996222dbc91d9ce1f1d21b4ea104f37f
janvikamilla <janvikamilla@gmail.com>
2026-09-18 01:14:56 +0530

    Create 07_remainder.c

---
+++ 07_remainder.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, remainder;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    remainder = a % b;
+    printf("Remainder = %d", remainder);
+    return 0;
+}
