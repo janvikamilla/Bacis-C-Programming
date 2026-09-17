b8eef438ed6fa2e4de86dbc2f3ae875c2ae838a4
janvikamilla <janvikamilla@gmail.com>
2026-09-18 00:50:15 +0530

    Create 04_subtraction.c

---
+++ 04_subtraction.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int a, b, difference;
+printf("Enter first number:");
+scanf("%d", &a);
+printf("Enter second number:");
+scanf("%d",&b);
+difference = a-b;
+printf("Difference = %d",difference);
+return 0;
+}
