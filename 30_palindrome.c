ffa391952b936b0cfe144861d5b654fe58e57948
janvikamilla <janvikamilla@gmail.com>
2026-09-18 19:57:22 +0530

    Create 30_palindrome.c

---
+++ 30_palindrome.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+    int n, original, reverse = 0, digit;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    original = n;
+    while(n != 0)
+    {
+        digit = n % 10;
+        reverse = reverse * 10 + digit;
+        n = n / 10;
+    }
+    if(original == reverse)
+    {
+        printf("Number is palindrome");
+    }
+    else
+    {
+        printf("Number is not palindrome");
+    }
+    return 0;
+}
