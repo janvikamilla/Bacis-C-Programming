5ed423bb9bf2c4e5847bd03d19892c0f5ba5cae4
Janvi Kamilla <janvikamilla@gmail.com>
2026-09-25 00:43:10 +0530

    Create 43_uppercase_lowercase.c

---
+++ 43_uppercase_lowercase.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+    if(ch >= 'A' && ch <= 'Z')
+    {
+        printf("Character is uppercase");
+    }
+    else if(ch >= 'a' && ch <= 'z')
+    {
+        printf("Character is lowercase");
+    }
+    else
+    {
+        printf("It is not an alphabet");
+    }
+    return 0;
+}
