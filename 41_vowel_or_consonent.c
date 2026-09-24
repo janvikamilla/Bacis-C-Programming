2989a6071a3e31c2b3ab9fefa6183b929877c61d
Janvi Kamilla <janvikamilla@gmail.com>
2026-09-25 00:40:32 +0530

    Create 41_vowel_or_consonent.c

---
+++ 41_vowel_or_consonent.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  char ch;
+printf("Enter a character:");
+scanf("%d", &ch);
+if(ch =='a"||ch =='e'||ch=='i'||ch=='o'||ch=='u')
+ {
+        printf("It is a vowel");
+    }
+    else
+    {
+        printf("It is a consonant");
+    }
+
+    return 0;
+}
